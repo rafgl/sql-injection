3d8fec0236e0d9c446f4a68e7e3d16ea24cfb26a
rafgl <rafael.luan.gaedke@hotmail.com>
2021-07-06 22:05:35 -0300

    update codes

--- code.php
+++ code.php
@@ -1,5 +1,5 @@
 <?php
-include('nta_db.php');
+include('db.php');
 $con = mysqli_connect($dbsrvname, $dbusername, $dbpassword, $dbname);
 if (!$con){
   echo('Connection ERROR');
@@ -9,13 +9,7 @@ $query = "SELECT * FROM student WHERE pid='" .$_POST['pid'] . "' AND password='"
 mysqli_multi_query($con, $query);
 $result = mysqli_store_result($con);
 if (mysqli_num_rows($result) > 0){
-	echo "<table border='1'>
-	<tr>
-	<th>PID</th>
-	<th>Name</th>
-	<th>Roll</th>
-	<th>Branch</th>
-	</tr>";
+
 
 	while($row = mysqli_fetch_array($result))
 	{
@@ -28,11 +22,11 @@ if (mysqli_num_rows($result) > 0){
 	}
 	echo "</table>";
 
-	die('Successfully Logged IN');
+	die('Sucesso ao logar');
 	mysqli_free_result($result);
 }
 else{
-  die('Wrong username or password');
+  die('Nome de usuário ou senha errada');
 }
 mysqli_close();
 ?>
--- nta1/nta_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbname = "class";
-$dbusername = "root";
-$dbpassword = "";
-$dbsrvname = "127.0.0.1";
-?>
